76fb902e75bb05411fb9ec416adb3a96307ba804
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-06-20 22:55:28 +0800

    learn

--- Main.java
+++ Main.java
@@ -1,36 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        /*
-          Write a Java program that takes a string input,
-          trims it, checks if it starts with "Hello",
-          and prints the position of the word 
-          "world" in the string.
-         */
-
-
-
-         /* Input: 
-            "   Hello, world!   "
-         
-            Output:  
-            Trimmed String: "Hello, world!" 
-            Starts with "Hello": true 
-            Position of "world": 8
-         */
-
-         //Code!!!
-
-        String[] unknown = {"   Hello, world!   "};
-        List<String> list1 = new ArrayList<String>();
-
-        for (String x : unknown){
-            list1.add(x.trim());
-            if (x.startsWith("Hello")){
-                for (int i = 0; i < list1.size(); i++){
-                    list1.get()
-                }
-            }
+    public static void main(String[] args) {
+        for (int x = 5; x > 1; x--){
+            System.out.println(x);
         }
     }
 }
