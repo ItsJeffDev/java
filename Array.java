269dc3dd75f185266fb97f54e35d46d730faced7
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-06-27 10:59:47 +0800

    learn array again

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        String student_Name[] = {"Jeff", "Reyjan", "May", "Ann", "Jazhmine"};
+
+        String number_Name[] = new String[5];
+
+        number_Name[0] = "Damn";
+
+        System.out.println(number_Name[0]);
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<String> foods = new ArrayList<String>();
+        while (true) {
+            System.out.print("Enter: ");
+            String user = sc.nextLine();
 
-        foods.add("Burger");
-        foods.add("Spaghetti");
-        foods.add("Carbonara");
+            foods.add(user);
+            System.out.println();
 
-        foods.set(0, "Damn");
-        foods.remove(1);
-        foods.clear();
+            for (int i = 0; i < foods.size(); i++) {
+                System.out.println("----------");
+                System.out.println(foods.get(i));
+                System.out.println("----------");
 
-
-        for (int i = 0; i < foods.size(); i++){
-            
-            System.out.println(foods.get(i));
+                if (Sta)
+            }
         }
-
     }
 }
